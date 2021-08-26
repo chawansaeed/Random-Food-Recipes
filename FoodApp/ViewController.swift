8e44106939300025ab9eca9d30fa7bb406b7e50f
Chawan Saeed <chawan.saeed02@gmail.com>
2021-08-26 22:55:36 +0300

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Chawan Saeed on 8/26/21.
+//  Copyright © 2021 Chawan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
